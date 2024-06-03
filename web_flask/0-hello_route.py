4c7ceb14c556978f49825536754f639e57d2477c
Awinimaxwelll <awinimaxwell428@gmail.com>
2024-06-03 08:47:00 -0700

    starting up flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Starting up flask
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+	"""displays hello HNBN"""
+	return "Hello HNBN!"
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port='5000')
