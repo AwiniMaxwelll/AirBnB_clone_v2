2abaca13ded2918abccaa65136889662a45d15df
Awinimaxwelll <awinimaxwell428@gmail.com>
2024-06-18 15:46:56 -0700

    flask continue

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,7 +2,6 @@
 """
 Starting up flask
 """
-
 from flask import Flask
 app = Flask(__name__)
 
@@ -12,5 +11,6 @@ def index():
 	"""displays hello HNBN"""
 	return "Hello HNBN!"
 
+
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port='5000')
+	app.run(host='0.0.0.0', port=5000)
